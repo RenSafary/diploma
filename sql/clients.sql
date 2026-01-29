24242b43df1864546ee9d7452a1ecc21305eb536
RenSafary <102967188+RenSafary@users.noreply.github.com>
2026-01-29 19:01:34 +0500

    + username passwd

--- sql/clients.sql
+++ sql/clients.sql
@@ -1,6 +1,8 @@
 CREATE TABLE clients
 (
     Id SERIAL PRIMARY KEY,
+    Username VARCHAR(250),
+    Passwd VARCHAR(250),
     FirstName VARCHAR(100),
     LastName VARCHAR(100),
     Email VARCHAR(250),
