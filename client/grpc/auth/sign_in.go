cc55cf4925481102c4d63abfdb5eb83641d7945b
RenSafary <102967188+RenSafary@users.noreply.github.com>
2026-01-29 13:04:12 +0500

    grpc auth client

---
+++ client/grpc/auth/sign_in.go
@@ -0,0 +1,34 @@
+package grpc_auth
+
+import (
+	"context"
+	authpb "diploma/proto/auth"
+	"google.golang.org/grpc"
+	"log"
+	"time"
+)
+
+func GRPC_SignIn(username, password string) (bool, string) {
+	conn, err := grpc.Dial("auth:50051", grpc.WithInsecure())
+	if err != nil {
+		log.Println("Couldn't connect to gRPC server", err)
+		return false, ""
+	}
+	defer conn.Close()
+
+	client := authpb.NewAuthServiceClient(conn)
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	resp, err := client.SignIn(ctx, &authpb.SignInRequest{
+		Username: username,
+		Password: password,
+	})
+	if err != nil {
+		log.Println("Couldn't connect to gRPC server", err)
+		return false, ""
+	}
+
+	return resp.Status, resp.Token
+}
