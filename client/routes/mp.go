f4622d8a84b64a682ccc1cb467e14dd4ab6795c2
RenSafary <102967188+RenSafary@users.noreply.github.com>
2026-01-29 13:55:33 +0500

    redirect to sign in
    Redirect if there's no user's token

--- client/routes/mp.go
+++ client/routes/mp.go
@@ -7,6 +7,11 @@ import (
 )
 
 func MainPage(w http.ResponseWriter, r *http.Request) {
+	if _, err := r.Cookie("token"); err != nil {
+		http.Redirect(w, r, "/sign-in", http.StatusPermanentRedirect)
+		return
+	}
+
 	tmpl, err := template.ParseFiles("./templates/main.html")
 	if err != nil {
 		log.Println("Couldn't parse HTML 'main'... ", err)
