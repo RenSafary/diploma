b4df92ced23a01b9069734c3032f49d24b118610
RenSafary <102967188+RenSafary@users.noreply.github.com>
2026-01-30 11:32:53 +0500

    sign up handlers

--- client/main.go
+++ client/main.go
@@ -15,11 +15,16 @@ func main() {
 	static := http.FileServer(http.Dir("./static"))
 	r.PathPrefix("/static/").Handler(http.StripPrefix("/static/", static))
 
+	// main page
 	r.HandleFunc("/", routes.MainPage).Methods("GET")
 
 	// authentication
+	// sign in
 	r.HandleFunc("/sign-in", auth.SignInForm).Methods("GET")
 	r.HandleFunc("/sign-in", auth.SignInPost).Methods("POST")
+	//sign up
+	r.HandleFunc("sign-up", auth.SignUpForm).Methods("GET")
+	r.HandleFunc("/sign-up", auth.SignUpPost).Methods("POST")
 
 	log.Println("Server is started on port :8080")
 	if err := http.ListenAndServe(":8080", r); err != nil {
