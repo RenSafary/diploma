f9959e083e83ded72f8bef8c2f821b0d248dd154
RenSafary <102967188+RenSafary@users.noreply.github.com>
2026-01-30 12:22:16 +0500

    SignUpPost handler

--- client/routes/auth/sign_up.go
+++ client/routes/auth/sign_up.go
@@ -1,6 +1,8 @@
 package auth
 
 import (
+	grpc_auth "diploma/client/grpc/auth"
+	"encoding/json"
 	"html/template"
 	"log"
 	"net/http"
@@ -15,4 +17,40 @@ func SignUpForm(w http.ResponseWriter, r *http.Request) {
 	}
 	w.Header().Set("Content-Type", "text/html")
 	tmpl.Execute(w, nil)
+}
+
+func SignUpPost(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	if err := r.ParseForm(); err != nil {
+		http.Error(w, "Unable to parse form", http.StatusBadRequest)
+		return
+	}
+
+	username := r.FormValue("username")
+	password := r.FormValue("password")
+	firstname := r.FormValue("firstname")
+	lastname := r.FormValue("lastname")
+	email := r.FormValue("email")
+	age := r.FormValue("age")
+	sex := r.FormValue("sex")
+
+	if sex == "Мужской" {
+		sex = "М"
+	} else {
+		sex = "Ж"
+	}
+
+	status, token := grpc_auth.GRPC_SignUp(username, password, firstname, lastname, email, sex, age)
+	if !status {
+		http.Error(w, "Couldn't sign up", http.StatusUnauthorized)
+		return
+	}
+
+	response := map[string]interface{}{
+		"status": status,
+		"token":  token,
+	}
+
+	json.NewEncoder(w).Encode(response)
 }
