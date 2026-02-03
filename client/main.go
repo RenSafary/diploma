4afca71d53b1be7f43b9c02874fdf6c205e8b87d
RenSafary <102967188+RenSafary@users.noreply.github.com>
2026-02-03 19:32:53 +0500

    sign-in/put-token route

--- client/main.go
+++ client/main.go
@@ -12,22 +12,20 @@ import (
 func main() {
 	r := mux.NewRouter()
 
+	// Static
 	static := http.FileServer(http.Dir("./static"))
 	r.PathPrefix("/static/").Handler(http.StripPrefix("/static/", static))
 
-	// main page
+	// Main
 	r.HandleFunc("/", routes.MainPage).Methods("GET")
 
-	// authentication
-	// sign in
+	// Authorization
 	r.HandleFunc("/sign-in", auth.SignInForm).Methods("GET")
 	r.HandleFunc("/sign-in-ws", auth.SignInWS)
-	//sign up
-	r.HandleFunc("/sign-up", auth.SignUpForm).Methods("GET")
-	r.HandleFunc("/sign-up", auth.SignUpPost).Methods("POST")
+	r.HandleFunc("/sign-in/put-token", auth.PutToken).Methods("POST")
 
 	log.Println("Server is started on port :8080")
 	if err := http.ListenAndServe(":8080", r); err != nil {
-		log.Fatal("Couldn't start the server: ", err)
+		log.Fatal("Couldn't start the server:", err)
 	}
 }
