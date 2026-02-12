46e49ffe325adba2d9526432dfbbf5ebc5911263
RenSafary <102967188+RenSafary@users.noreply.github.com>
2026-02-12 18:20:21 +0500

    GetAllUsers grpc client

---
+++ gateway/grpc/users/get_user.go
@@ -0,0 +1,36 @@
+package grpc_users
+
+import (
+	"context"
+	userspb "diploma/proto/users"
+	"fmt"
+	"log"
+	"time"
+
+	"google.golang.org/grpc"
+)
+
+func GRPC_Get_All_Users() ([]*userspb.User, error) {
+	conn, err := grpc.Dial("users:50053", grpc.WithInsecure())
+	if err != nil {
+		return nil, err
+	}
+	defer conn.Close()
+
+	client := userspb.NewUsersServiceClient(conn)
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	resp, err := client.GetAllUsers(ctx, &userspb.GetAllUsersRequest{})
+	if err != nil {
+		return nil, err
+	}
+	log.Println("users from grpc", len(resp.Users))
+
+	if resp == nil {
+		return nil, fmt.Errorf("empty grpc response")
+	}
+
+	return resp.Users, nil
+}
