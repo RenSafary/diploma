b4d8ed7b876e42af3087e60d4bde52a847022863
RenSafary <102967188+RenSafary@users.noreply.github.com>
2026-02-12 18:20:10 +0500

    GetAllUsers handler

--- gateway/routes/admin-panel/roles/users/service.go
+++ gateway/routes/admin-panel/roles/users/service.go
@@ -1,9 +1,52 @@
 package roles
 
+import (
+	grpc_users "diploma/gateway/grpc/users"
+	userspb "diploma/proto/users"
+	"html/template"
+	"net/http"
+)
+
+type PageData struct {
+	Users []*userspb.User
+}
+
 func MakeEmployee() {
 
 }
 
 func RemoveEmployee() {
+
+}
+
+func GetAllUsers(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case http.MethodGet:
+		// get users' list
+		users, err := grpc_users.GRPC_Get_All_Users()
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		// parsing
+		tmpl, err := template.ParseFiles("./templates/admin/roles/users/GetAllUsers.html")
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		data := PageData{
+			Users: users,
+		}
+
+		w.Header().Set("Content-Type", "text/html")
+		if err := tmpl.Execute(w, data); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+	case http.MethodPost:
+		return
+	default:
+		http.Error(w, "method not allowed", http.StatusMethodNotAllowed)
+	}
 
 }
