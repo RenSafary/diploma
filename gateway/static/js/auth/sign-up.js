18638a087da652d24dc0ed2f13bb323e17e3506a
RenSafary <102967188+RenSafary@users.noreply.github.com>
2026-02-19 15:18:14 +0500

    ws

---
+++ gateway/static/js/auth/sign-up.js
@@ -0,0 +1,38 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const form = document.getElementById("sign-up-form");
+    const ws = new WebSocket("ws://localhost:8080/sign-up-ws");
+
+    ws.onopen = () => console.log("WebSocket connected");
+
+    ws.onmessage = (event) => {
+        const data = JSON.parse(event.data);
+
+        if (data.status) {
+            alert("Вход успешен!");
+
+            fetch("/put-token", {
+                method: "POST",
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify({ token: data.token })
+            })
+            .then(res => {
+                if (res.ok) {
+                    console.log("Token stored in cookie");
+                    window.location.href = "/"; 
+                } else {
+                    console.error("Failed to set token");
+                }
+            });
+        } else {
+            alert("Неверный логин или пароль");
+        }
+    };
+
+    form.addEventListener("submit", (e) => {
+        e.preventDefault();
+        ws.send(JSON.stringify({
+            username: form.username.value,
+            password: form.password.value
+        }));
+    });
+});
