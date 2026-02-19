5b3b6fd792a2b3829e9c764fba6c0369d54ee9ed
RenSafary <102967188+RenSafary@users.noreply.github.com>
2026-02-19 16:11:19 +0500

    give token

--- users-service/db/users.go
+++ users-service/db/users.go
@@ -98,3 +98,40 @@ func (r *Users) GetAllUsers() ([]*userspb.User, error) {
 	}
 	return users, nil
 }
+
+type UserToken struct {
+	Id       int
+	Username string
+	Password string
+	Admin    bool
+}
+
+func (d *Users) GiveToken(username, password string) (string, error) {
+	user := &UserToken{}
+
+	err := d.DB.QueryRow(
+		"SELECT id, passwd, adm FROM users WHERE username=$1", username,
+	).Scan(&user.Id, &user.Password, &user.Admin)
+
+	if err == sql.ErrNoRows {
+		log.Println("User does not exist: ", err)
+		return "", err
+	} else if err != nil {
+		log.Println(err)
+		return "", err
+	}
+
+	// comparing password
+	err = utils.CompareHashPass(user.Password, password)
+	if err != nil {
+		return "Wrong login or password", nil
+	}
+
+	// giving jwt token
+	token, err := utils.GenerateToken(user.Id, username, user.Admin)
+	if err != nil {
+		return "Wrong login or password", nil
+	}
+
+	return token, nil
+}
